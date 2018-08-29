913d05a1f5b34dc83fcdfbb493eee9ebb9828bb3
gabriel <ganiserb@gmail.com>
2018-08-28 21:16:03 -0300

    Moved code around and better representation

--- breadth_first_graph.py
+++ breadth_first_graph.py
@@ -1,3 +1,20 @@
+# Graph search
+# ~ function BREADTH-FIRST-SEARCH(problem) returns a solution, or failure
+# ~ node ←a node with STATE = problem.INITIAL-STATE, PATH-COST = 0
+# ~ if problem.GOAL-TEST(node.STATE) then return SOLUTION(node)
+# ~ frontier ← a FIFO queue with node as the only element
+# ~ explored ← an empty set
+# ~ loop do
+    # ~ if EMPTY?(frontier ) then return failure
+    # ~ node ← POP(frontier ) /* chooses the shallowest node in frontier */
+    # ~ add node.STATE to explored
+    # ~ for each action in problem.ACTIONS(node.STATE) do
+        # ~ child ← CHILD-NODE(problem, node, action)
+        # ~ if child.STATE is not in explored or frontier then
+            # ~ if problem.GOAL-TEST(child.STATE) then return SOLUTION(child)
+            # ~ frontier ← INSERT(child,frontier )
+
+# Tree search
 # ~ function BREADTH-FIRST-SEARCH(problem) returns a solution, or failure
 # ~ node ←a node with STATE = problem.INITIAL-STATE, PATH-COST = 0
 # ~ if problem.GOAL-TEST(node.STATE) then return SOLUTION(node)
@@ -16,22 +33,87 @@
 class Node(object):
     parent = None
     state = None
+    action = None
     
-    def __init__(self, state, parent=None):
+    def __init__(self, state, parent=None, action=None):
         self.parent = parent
         self.state = state
+        self.action = action
         
     def __repr__(self):
-        rep = ""
+        # Build a list with the path to the solution
+        nodes = []
         node = self
         while node:
-            rep = rep + "\n | \n" + str(node.state)
+            nodes.append(node)
             node = node.parent
-        return rep
-                
+            
+        # Make a nice printable string out of that list
+        representation = ""
+        for node in reversed(nodes):
+            if node:
+                action = node.action if node and node.action and node.parent else " "
+                representation = "{}\n | \n | ({}) \n v \n{}".format(representation, action, str(node.state))
+        return representation
+
+
+class BreadthFirstTreeSearch(object):
+    problem = None
+    
+    def __init__(self, problem):
+        self.problem = problem
+    
+    def run(self):
+        node = Node(self.problem.initial)
+        if self.problem.is_goal(node.state):
+            return node
+        frontier = [node]
+
+        while True:
+            if not frontier:
+                raise Exception("Empty frontier. Solution not found")
+
+            node = frontier.pop(0)
+            for action in self.problem.actions(node.state):
+                child_state = self.problem.result(node.state, action)
+                child = Node(child_state, node, action)
+                if child.state not in [n.state for n in frontier]:
+                    if self.problem.is_goal(child.state):
+                        return child
+                    frontier.append(child)
+
+
+class BreadthFirstGraphSearch(object):
+    problem = None
+    
+    def __init__(self, problem):
+        self.problem = problem
+    
+    def run(self):
+        node = Node(self.problem.initial)
+        if self.problem.is_goal(node.state):
+            return node
+        frontier = [node]
+        explored = set()
+
+        while True:
+            if not frontier:
+                raise Exception("Empty frontier. Solution not found")
+
+            node = frontier.pop(0)
+            explored.add(node.state)
+            for action in self.problem.actions(node.state):
+                child_state = self.problem.result(node.state, action)
+                child = Node(child_state, node)
+                if child.state not in explored or child.state not in [n.state for n in frontier]:
+                    if self.problem.is_goal(child.state):
+                        return child
+                    frontier.append(child)
+
 
 class OchoProblem(object):
-    initial = "312\n045\n678"
+    #initial = "312\n045\n678"
+    initial = "312\n065\n748"
     
     def find_number(self, state, number):
         # Returns a tuple containing the row and column number of 0
@@ -73,63 +155,7 @@ class OchoProblem(object):
         b = a.replace(action, "0")
         c = b.replace("x", action)
         return c
-        
 
-class BreadthFirstGraphSearch(object):
-    problem = None
-    
-    def __init__(self, problem):
-        self.problem = problem
-    
-    def run(self):
-        node = Node(self.problem.initial)
-        if self.problem.is_goal(node.state):
-            return node
-        frontier = [node]
-        explored = set()
-
-        while True:
-            if not frontier:
-                raise Exception("Empty frontier. Solution not found")
-
-            node = frontier.pop(0)
-            explored.add(node.state)
-            for action in self.problem.actions(node.state):
-                child_state = self.problem.result(node.state, action)
-                child = Node(child_state, node)
-                if child.state not in explored or child.state not in [n.state for n in frontier]:
-                    if self.problem.is_goal(child.state):
-                        return child
-                    frontier.append(child)
-
-
-class BreadthFirstGraphSearch(object):
-    problem = None
-    
-    def __init__(self, problem):
-        self.problem = problem
-    
-    def run(self):
-        node = Node(self.problem.initial)
-        if self.problem.is_goal(node.state):
-            return node
-        frontier = [node]
-        explored = set()
-
-        while True:
-            if not frontier:
-                raise Exception("Empty frontier. Solution not found")
-
-            node = frontier.pop(0)
-            explored.add(node.state)
-            for action in self.problem.actions(node.state):
-                child_state = self.problem.result(node.state, action)
-                child = Node(child_state, node)
-                if child.state not in explored or child.state not in [n.state for n in frontier]:
-                    if self.problem.is_goal(child.state):
-                        return child
-                    frontier.append(child)
-                    
 
 if __name__ == '__main__':
     problem = OchoProblem()
@@ -140,7 +166,7 @@ if __name__ == '__main__':
     print("Initial:")
     print(problem.initial)
     
-    search = BreadthFirstGraphSearch(problem)
+    search = BreadthFirstTreeSearch(problem)
     solution = search.run()
 
     print("Solution:")
