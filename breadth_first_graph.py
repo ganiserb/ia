afd6cff6b61e9cf334e7bf7e0885425bc67f52b4
gabriel <ganiserb@gmail.com>
2018-08-28 21:17:36 -0300

    Removed useless comment

--- breadth_first_graph.py
+++ breadth_first_graph.py
@@ -14,22 +14,6 @@
             # ~ if problem.GOAL-TEST(child.STATE) then return SOLUTION(child)
             # ~ frontier ← INSERT(child,frontier )
 
-# Tree search
-# ~ function BREADTH-FIRST-SEARCH(problem) returns a solution, or failure
-# ~ node ←a node with STATE = problem.INITIAL-STATE, PATH-COST = 0
-# ~ if problem.GOAL-TEST(node.STATE) then return SOLUTION(node)
-# ~ frontier ← a FIFO queue with node as the only element
-# ~ explored ← an empty set
-# ~ loop do
-    # ~ if EMPTY?(frontier ) then return failure
-    # ~ node ← POP(frontier ) /* chooses the shallowest node in frontier */
-    # ~ add node.STATE to explored
-    # ~ for each action in problem.ACTIONS(node.STATE) do
-        # ~ child ← CHILD-NODE(problem, node, action)
-        # ~ if child.STATE is not in explored or frontier then
-            # ~ if problem.GOAL-TEST(child.STATE) then return SOLUTION(child)
-            # ~ frontier ← INSERT(child,frontier )
-
 class Node(object):
     parent = None
     state = None
